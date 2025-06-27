d32e8b6e9437403570de0ba0152b12fd93eaa0fd
louis <louis.vanstappen@gmail.com>
2025-06-27 14:57:24 +0200

    Added wandb

--- decoder_experiment.py
+++ decoder_experiment.py
@@ -1,10 +1,13 @@
 import math
 
 import torch
+import wandb
 from transformers import AutoModel, AutoVideoProcessor
 
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 from models.decoder import VJEPA2Decoder
+
+run = wandb.init(project="vjepa2-decoder-experiment")
 
 hf_repo = "facebook/vjepa2-vitl-fpc64-256"
 
@@ -25,9 +28,9 @@ dataset = LeRobotDataset("lerobot/droid_100", delta_timestamps=delta_timestamps)
 optimizer = torch.optim.AdamW(decoder.parameters(), lr=1e-4, weight_decay=0.1)
 dataloader = torch.utils.data.DataLoader(
     dataset,
-    num_workers=4,
-    batch_size=1024,
-    # batch_size=2,
+    # num_workers=4,
+    # batch_size=1024,
+    batch_size=1,
     shuffle=True,
     pin_memory=device.type != "cpu",
     drop_last=True,
@@ -46,7 +49,8 @@ dataloader = torch.utils.data.DataLoader(
 # scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda, last_epoch=-1)
 
 decoder.train()
-num_steps = 10
+num_steps = 2000
+run.watch(decoder)
 for step in range(num_steps):
     for batch in dataloader:
         images = batch[camera_key]
@@ -54,8 +58,6 @@ for step in range(num_steps):
 
         outputs = model(pixel_values, skip_predictor=True)
         reconstructed = decoder(outputs.last_hidden_state)
-
-        print(f"Reconstructed shape: {reconstructed.shape}, Pixel values shape: {pixel_values.shape}")
 
         loss = torch.nn.functional.mse_loss(reconstructed, pixel_values)
         loss.backward()
@@ -66,6 +68,7 @@ for step in range(num_steps):
         optimizer.zero_grad()
 
         print(f"Step {step + 1}/{num_steps}, Loss: {loss.item()}")
+        run.log({"loss": loss.item()})
 
     # scheduler.step()
     torch.save(decoder.state_dict(), f"decoder_{step}.pt")
